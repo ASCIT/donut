f14ea12e9925416738244e3b59d2b8c32cbe1713
Eric Pelz <eric+github@ericpelz.com>
2015-02-04 02:13:53 -0800

    Small change in test imports
    - We're trying to use `import flask` instead of `from flask import ...`
      throughout the repo, so I'm switching to that in the tests too.
    - Use `from Donut.tests.fixtures import *` in module tests to be more
      consistent with blueprint tests.

--- Donut/modules/example/tests/test_routes.py
+++ Donut/modules/example/tests/test_routes.py
@@ -1,10 +1,10 @@
-from flask import url_for
+import flask
 
 from Donut.tests.fixtures import *
 from Donut import app
 
 def testHome(client):
-  rv = client.get(url_for('example.home'))
+  rv = client.get(flask.url_for('example.home'))
 
   assert rv.status_code == 200
   assert "Example page" in rv.data
