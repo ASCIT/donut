7fe50e6d10bc0fdc2f8671e00629c350612f7e83
David Qu <davidqu12345@gmail.com>
2015-09-23 17:10:11 -0700

    Added arg parser to take in port as argument for run_server.py

--- run_server.py
+++ run_server.py
@@ -2,9 +2,17 @@
 # uses wsgi to start up, and bypasses this file. So we are free to have debug
 # settings enabled.
 from Donut import app, config
+import argparse
+
+parser = argparse.ArgumentParser(description='Set up development server.')
+parser.add_argument('port_num', metavar='port_num', type=int, 
+                    help='Port on which to run server.')
+
+args = parser.parse_args()
+
 
 if __name__ == "__main__":
-    port = getattr(config, 'PORT', 5050)
+    port = getattr(config, 'PORT', args.port_num)
     debug = getattr(config, 'DEBUG', True)
     app.run(debug=debug, port=port)
 
