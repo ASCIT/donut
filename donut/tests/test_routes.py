52bf7bb5d755f7dc0fde193d29d82306b2426b71
David Qu <davidqu12345@gmail.com>
2015-09-25 18:18:06 -0700

    Renamed test function to follow convention

--- donut/tests/test_routes.py
+++ donut/tests/test_routes.py
@@ -3,7 +3,7 @@ import flask
 from donut.tests.fixtures import client
 from donut import app
 
-def testHome(client):
+def test_home(client):
   rv = client.get(flask.url_for('home'))
 
   assert rv.status_code == 200
