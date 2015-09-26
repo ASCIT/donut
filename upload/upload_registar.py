c661c3bd80172ac9b4f2cda06391bd69ac3f750e
David Qu <davidqu12345@gmail.com>
2015-09-25 18:57:59 -0700

    Template for upload script.

---
+++ upload/upload_registar.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                description="Manages importing registrar data each term")
+    # TODO parse sample_registrar_data.txt and upload to db. 
