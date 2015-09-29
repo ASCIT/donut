927760b277665c8178d4f3ab5fdac31fc9e8c8b5
David Qu <davidqu12345@gmail.com>
2015-09-28 20:28:28 -0700

    Fixed space.

--- donut/modules/auth/routes.py
+++ donut/modules/auth/routes.py
@@ -16,12 +16,12 @@ def login_submit():
 
 @blueprint.route('/userreg')
 def user_reg():
-	return flask.render_template('userreg.html')
+    return flask.render_template('userreg.html')
 
 @blueprint.route('/userreg/submit', methods = ['POST'])
 def user_reg_submit():
-	return ""
+    return ""
 
 @blueprint.route('/recover', methods = ['POST'])
 def recover():
-	return ""
+    return ""
