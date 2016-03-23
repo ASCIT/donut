a0bc5e68bc6c856cedb06f32bf030b0f7db74a21
David Qu <davidqu12345@gmail.com>
2016-03-23 12:36:07 +0000

    Added Environment class for database configuration.

---
+++ donut/environment.py
@@ -0,0 +1,27 @@
+class Environment(object):
+  """A configuration environment.
+  Provides environment-specific configuration variables:
+    db_uri: uri to connect to the database.
+    db_hostname: database hostname.
+    db_name: name of the database.
+    db_user: database user.
+    db_password: database password.
+    debug: bool for whether or not debug mode should be enabled.
+    testing: bool for whether or not testing mode should be enabled.
+    secret_key: secret key for session cookie.
+  """
+
+  def __init__(self, db_hostname, db_name, db_user, db_password, debug,
+      testing, secret_key):
+    self.db_hostname = db_hostname
+    self.db_name = db_name
+    self.db_user = db_user
+    self.db_password = db_password
+    self.debug = debug
+    self.testing = testing
+    self.secret_key = secret_key
+
+  @property
+  def db_uri(self):
+    return "mysql://{0}:{1}@{2}/{3}".format(
+        self.db_user, self.db_password, self.db_hostname, self.db_name)
