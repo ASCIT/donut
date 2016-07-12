dbb1e43917bfc6280d242cc9b53900687a8b3954
David Qu <davidqu12345@gmail.com>
2016-07-12 15:37:13 +0000

    Add setup.sql and reset.sql to load Travis test db

--- sql/donut.sql
+++ sql/donut.sql
@@ -68,7 +68,3 @@ CREATE TABLE users (
     UNIQUE KEY (username)
 );
 
-
-/* Test data / initial data */
-INSERT INTO members(uid, last_name, first_name, email)
-    VALUES ('1957540', 'Qu', 'David', 'davidqu12345@gmail.com');
