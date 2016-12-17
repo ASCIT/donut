b9e3e0279ade437e9440c2248db01cfb636c92d4
David Qu <davidqu12345@gmail.com>
2016-12-17 19:15:35 +0000

    Set 2 space indent.

--- donut/modules/auth/routes.py
+++ donut/modules/auth/routes.py
@@ -4,24 +4,24 @@ from donut.modules.auth import blueprint
 
 @blueprint.route('/login')
 def login():
-    """Display login page."""
-    return flask.render_template('login.html')
+  """Display login page."""
+  return flask.render_template('login.html')
 
 @blueprint.route('/login/submit', methods=['POST'])
 def login_submit():
-    """Handle authentication."""
-    # TODO: Add login logic.
-    flask.flash('Incorrect username or password. Please try again!')
-    return flask.redirect(flask.url_for('auth.login'))
+  """Handle authentication."""
+  # TODO: Add login logic.
+  flask.flash('Incorrect username or password. Please try again!')
+  return flask.redirect(flask.url_for('auth.login'))
 
 @blueprint.route('/userreg')
 def user_reg():
-    return flask.render_template('userreg.html')
+  return flask.render_template('userreg.html')
 
 @blueprint.route('/userreg/submit', methods = ['POST'])
 def user_reg_submit():
-    return ""
+  return ""
 
 @blueprint.route('/recover', methods = ['POST'])
 def recover():
-    return ""
+  return ""
