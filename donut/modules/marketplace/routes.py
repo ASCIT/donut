7a37eb91af1a12a8f0e01d1a299eccc3d719a0a2
Thomas Leing <bluezebragames@gmail.com>
2017-06-22 04:46:06 +0000

    Fixed link from index.html to /marketplace

--- donut/modules/marketplace/routes.py
+++ donut/modules/marketplace/routes.py
@@ -3,8 +3,8 @@ import flask
 from donut.modules.marketplace import blueprint, helpers
 
 @blueprint.route('/marketplace')
-def login():
-  """Display login page."""
+def marketplace():
+  """Display marketplace page."""
   return flask.render_template('marketplace.html')
 
 
