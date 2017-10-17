df3f94e96e5c28f8eff503b3ce264b2cf0cda209
Sean Yu <sean@donut.caltech.edu>
2017-10-17 00:57:24 +0000

    Make lint

--- donut/modules/groups/constants.py
+++ donut/modules/groups/constants.py
@@ -1,10 +1,11 @@
 from enum import Enum
 
+
 # Enum for group types in the groups table
 class GroupTypes(Enum):
     HOUSE = 'house'
-    COMMITTEE = 'committee' # i.e IHC, BOC, CRC
-    ASCIT = 'ascit' # For all groups that are involved with ascit
-    PUBLICATION = 'publication' # i.e The Tech
-    UGCURRENT = 'ugcurrent' # current student groups i.e ug2020
-    UGALUMN = 'ugalumn' # alumn groups, i.e ug2010 
+    COMMITTEE = 'committee'  # i.e IHC, BOC, CRC
+    ASCIT = 'ascit'  # For all groups that are involved with ascit
+    PUBLICATION = 'publication'  # i.e The Tech
+    UGCURRENT = 'ugcurrent'  # current student groups i.e ug2020
+    UGALUMN = 'ugalumn'  # alumn groups, i.e ug2010
