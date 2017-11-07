c26368b6d66b53fcbfe84389f1c4a12d148d52bf
Thomas Leing <bluezebragames@gmail.com>
2017-11-07 01:16:44 +0000

    So this was useless anyway

--- donut/modules/marketplace/routes.py
+++ donut/modules/marketplace/routes.py
@@ -448,26 +448,3 @@ def sell():
 
         return helpers.render_with_top_marketplace_bar(
             'sell/sell_4.html', result=result)
-
-
-@blueprint.route('/1/marketplace_items')
-def get_marketplace_items_list():
-    """GET /1/marketplace_items/"""
-
-    # Create a dict of the passed in attributes which are filterable
-    filterable_attrs = [
-        "item_id", "cat_id", "user_id", "item_title", "item_details",
-        "item_images", "item_condition", "item_price", "item_timestamp",
-        "item_active", "textbook_id", "textbook_isbn", "textbook_edition"
-    ]
-    attrs = {
-        tup: flask.request.args[tup]
-        for tup in flask.request.args if tup in filterable_attrs
-    }
-    # Get the fields to return if they were passed in
-    fields = None
-    if "fields" in flask.request.args:
-        fields = [f.strip() for f in flask.request.args["fields"].split(',')]
-
-    return json.dumps(
-        helpers.get_marketplace_items_list_data(fields=fields, attrs=attrs))
