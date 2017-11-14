8c92292648e38374e00598cbc66cb0b073fdf076
Thomas Leing <bluezebragames@gmail.com>
2017-11-14 02:05:23 +0000

    More test changes

--- Makefile
+++ Makefile
@@ -12,8 +12,7 @@ pip-install:
 lint:
 	yapf -i -r .
 
-test:
-	make init-test-db && \
+test: init-test-db
 	python3 -m pytest .
 
 init-test-db:
--- tests/modules/marketplace/test_marketplace_helpers.py
+++ tests/modules/marketplace/test_marketplace_helpers.py
@@ -1,7 +1,9 @@
 from donut.testing.fixtures import client
 from donut import app
 from donut.modules.marketplace.helpers import (get_category_name_from_id,
-                                               get_table_columns)
+                                               get_table_columns,
+                                               get_matches,
+                                               tokenize_query)
 
 
 def test_get_category_name_from_id(client):
@@ -21,3 +23,18 @@ def test_get_table_columns(client):
         'item_price', 'item_timestamp', 'item_active', 'textbook_id',
         'textbook_edition', 'textbook_isbn'
     ]
+
+
+def test_get_matches(client):
+    l1 = [1, 2, 3, 4]
+    l2 = [4, 3]
+    assert get_matches(l1, l2) == 2
+    assert get_matches(l2, l1) == 2
+    assert get_matches(l1, l1) == 4
+    l3 = []
+    assert get_matches(l3, l3) == 0
+
+
+def test_tokenize_query(client):
+    query = "and,if great testing testing ,.,..,/./,/,./,./,"
+    assert tokenize_query(query) == ['great', 'testing', 'testing']
