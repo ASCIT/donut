3abff174d46cbb3274e922e865173e28c652ff99
Sean Yu <sean@donut.caltech.edu>
2017-11-21 00:25:44 +0000

    Added new route to campus positions template, merged from master

--- donut/routes.py
+++ donut/routes.py
@@ -10,3 +10,7 @@ def home():
 @app.route('/contact')
 def contact():
     return flask.render_template('contact.html')
+
+@app.route('/campus_positions')
+def campus_positions():
+    return flask.render_template('campus_positions.html')
