6e6ab4ac6ede6235697ccb89b2159977974b4d7e
Caleb Sander <caleb.sander@gmail.com>
2017-11-27 17:52:45 -0800

    Preliminary support for room reservations

--- donut/modules/rooms/helpers.py
+++ donut/modules/rooms/helpers.py
@@ -1,6 +1,8 @@
 import flask
 import sqlalchemy
 
+from donut.auth_utils import get_user_id
+
 def get_rooms():
     """Gets a list of rooms in the form {id, name}"""
 
@@ -8,3 +10,17 @@ def get_rooms():
     rooms = flask.g.db.execute(query).fetchall()
 
     return [{"id": id, "name": location} for id, location in rooms]
+
+def is_room(room_id_string):
+    query = sqlalchemy.text("SELECT id FROM rooms WHERE id = :room_id")
+    room = flask.g.db.execute(query, room_id=int(room_id_string))
+    return room is not None
+
+def add_reservation(room, username, reason, start, end):
+    # TODO: Check that there are no overlapping reservations
+    insertion = sqlalchemy.text("""
+    INSERT INTO room_reservations
+    (room_id, user_id, reason, start_time, end_time)
+    VALUES (:room, :user, :reason, :start, :end)
+    """)
+    flask.g.db.execute(insertion, room=room, user=get_user_id(username), reason=reason, start=start, end=end)
--- donut/modules/rooms/routes.py
+++ donut/modules/rooms/routes.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 import flask
 
 from . import blueprint, helpers
+from donut.validation_utils import (validate_date, validate_exists, validate_in, validate_int)
 
+AM_OR_PM = set(["A", "P"])
 
 @blueprint.route("/reserve")
 def rooms_home():
@@ -13,4 +16,42 @@ def rooms_home():
 def book():
     """POST /1/book-room/"""
 
-    return flask.render_template("reservations.html", rooms=helpers.get_rooms())
+    form = flask.request.form
+    validations = [
+        validate_exists(form, "room") and validate_int(form["room"]) and helpers.is_room(form["room"]),
+        validate_exists(form, "date") and validate_date(form["date"]),
+        validate_exists(form, "start_hour") and validate_int(form["start_hour"], 1, 12),
+        validate_exists(form, "start_minute") and validate_int(form["start_minute"], 0, 59),
+        validate_exists(form, "start_period") and validate_in(form["start_period"], AM_OR_PM),
+        validate_exists(form, "end_hour") and validate_int(form["end_hour"], 1, 12),
+        validate_exists(form, "end_minute") and validate_int(form["end_minute"], 0, 59),
+        validate_exists(form, "end_period") and validate_in(form["end_period"], AM_OR_PM),
+        validate_exists(form, "reason")
+    ]
+
+    if not all(validations):
+        # TODO: Should include old parameters in form here
+        return flask.render_template("reservations.html", rooms=helpers.get_rooms())
+
+    start_hour = int(form["start_hour"]) % 12
+    if form["start_period"] == "P":
+        start_hour += 12
+    start_minute = int(form["start_minute"])
+    end_hour = int(form["end_hour"]) % 12
+    if form["end_period"] == "P":
+        end_hour += 12
+    end_minute = int(form["end_minute"])
+    if start_hour > end_hour or (start_hour == end_hour and start_minute >= end_minute):
+        flask.flash("Start time must be before end time")
+        # TODO: Should include old parameters in form here
+        return flask.render_template("reservations.html", rooms=helpers.get_rooms())
+
+    room = int(form["room"])
+    day = datetime.strptime(form["date"], '%Y-%m-%d')
+    start = datetime(day.year, day.month, day.day, start_hour, start_minute)
+    end = datetime(day.year, day.month, day.day, end_hour, end_minute)
+    reason = form["reason"] or None
+
+    helpers.add_reservation(room, flask.session["username"], reason, start, end)
+
+    return flask.render_template("reservation_success.html")
