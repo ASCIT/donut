514d02483cc5e61a8c1faa2999ff496e3c6e1270
Ziyan Mo <moziyan@yahoo.com>
2017-12-29 02:08:26 +0000

    changed routes, and helpers for new html files

--- donut/modules/committee_sites/helpers.py
+++ donut/modules/committee_sites/helpers.py
@@ -8,7 +8,7 @@ def get_BoC_member():
     """
     sqlText1 = "groups NATURAL JOIN positions NATURAL JOIN position_holders "
     sqlText2 = "NATURAL JOIN members"
-    s = sqlalchemy.sql.select(["first_name, last_name, pos_name"]).select_from(sqlalchemy.text(sqlText1 + sqlText2))
+    s = sqlalchemy.sql.select(["first_name, last_name, pos_name, email"]).select_from(sqlalchemy.text(sqlText1 + sqlText2))
 
     s = s.where(sqlalchemy.text("group_name = 'BoC'"))
     result = flask.g.db.execute(s)
@@ -16,12 +16,40 @@ def get_BoC_member():
     high_pos = []
     pos_house = []
     counter = 0
-    for fname, lname, pos_name in result:
+    for fname, lname, pos_name, email in result:
         name = fname + lname
         if pos_name == 'Chair' or 'ecretary' in pos_name:
-            high_pos.append((name, pos_name))
+            high_pos.append((name, pos_name, email))
         else:
-            pos_house.append((name, pos_name))
+            pos_house.append((name, pos_name, email))
+
+    high_pos.sort(key=lambda tup:tup[1])
+    pos_house.sort(key=lambda tup:tup[1])
+
+    fin_result = high_pos + pos_house
+
+    return fin_result
+
+def get_CRC_member():
+    """
+    Queries the database and returns list of people on CRC
+    """
+    sqlText1 = "groups NATURAL JOIN positions NATURAL JOIN position_holders "
+    sqlText2 = "NATURAL JOIN members"
+    s = sqlalchemy.sql.select(["first_name, last_name, pos_name, email"]).select_from(sqlalchemy.text(sqlText1 + sqlText2))
+
+    s = s.where(sqlalchemy.text("group_name = 'CRC'"))
+    result = flask.g.db.execute(s)
+
+    high_pos = []
+    pos_house = []
+    counter = 0
+    for fname, lname, pos_name, email in result:
+        name = fname + lname
+        if pos_name == 'Chair' or 'ecretary' in pos_name:
+            high_pos.append((name, pos_name, email))
+        else:
+            pos_house.append((name, pos_name, email))
 
     high_pos.sort(key=lambda tup:tup[1])
     pos_house.sort(key=lambda tup:tup[1])
--- donut/modules/committee_sites/routes.py
+++ donut/modules/committee_sites/routes.py
@@ -6,4 +6,30 @@ from donut.modules.committee_sites import blueprint, helpers
 @blueprint.route('/BoC')
 def boc():
     """Display Boc page."""
-    return flask.render_template('BoC.html', helpers.get_BoC_member())
+    return flask.render_template('BoC.html', BoC = helpers.get_BoC_member())
+
+@blueprint.route('/BoC/defendants')
+def defendants():
+    """Display Boc defendant page."""
+    return flask.render_template('BoC.defendants.html')
+
+@blueprint.route('/BoC/witnesses')
+def witnesses():
+    """Display Boc witnesses page."""
+    return flask.render_template('BoC.witnesses.html')
+
+@blueprint.route('/BoC/FAQ')
+def FAQ():
+    """Display Boc FAQ page."""
+    return flask.render_template('BoC.FAQ.html')
+
+@blueprint.route('/BoC/reporters')
+def reporters():
+    """Display Boc FAQ page."""
+    return flask.render_template('BoC.reporters.html')
+
+@blueprint.route('/CRC')
+def CRC():
+    """Display CRC page."""
+    return flask.render_template('CRC.html', CRC = helpers.get_CRC_member())
+
