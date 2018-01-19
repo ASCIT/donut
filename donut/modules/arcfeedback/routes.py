795e82f59daf5f0739b87c623f53c41c3bf58358
Davis Tardif <davoscavo@gmail.com>
2018-01-18 17:33:21 -0800

    Added route to post form

--- donut/modules/arcfeedback/routes.py
+++ donut/modules/arcfeedback/routes.py
@@ -8,4 +8,13 @@ def arcfeedback():
 
 @blueprint.route('/arcfeedback/submit', methods=['POST'])
 def arcfeedback_submit():
-    return
+    fields = ['name', 'email', 'class', 'msg']
+    data = {}
+    for field in fields:
+        data[field] = flask.request.form.get(field)
+    if data['class'] != "" and data['msg'] != "":
+        flask.flash('Success!')
+        return flask.redirect(flask.url_for('home'))
+    flask.flash('Class and feedback are required fields!')
+    return flask.redirect(flask.url_for('arcfeedback.arcfeedback'))
+    
