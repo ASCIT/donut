2eb8f3d9e47843d89abe5ae11b377cca9fee193f
Sean Yu <sean@donut.caltech.edu>
2018-02-08 17:25:17 -0800

    make lint

--- donut/routes.py
+++ donut/routes.py
@@ -5,6 +5,7 @@ from donut.auth_utils import get_user_id
 from donut.modules.core import helpers
 from donut.modules import groups
 
+
 @app.route('/')
 def home():
     return flask.render_template('donut.html')
@@ -14,24 +15,26 @@ def home():
 def contact():
     return flask.render_template('contact.html')
 
+
 @app.route('/campus_positions')
 def campus_positions():
     approved_group_ids = []
     approved_group_names = []
     try:
         user_id = get_user_id(flask.session['username'])
-        result = helpers.get_group_list_of_member(user_id) 
+        result = helpers.get_group_list_of_member(user_id)
         for res in result:
             if res["control"] == 1:
                 approved_group_ids.append(res["group_id"])
                 approved_group_names.append(res["group_name"])
     except KeyError:
         print("No One is Logged In!")
-    all_positions = groups.helpers.get_position_data();
+    all_positions = groups.helpers.get_position_data()
     for pos in all_positions:
-        pos['start_date'] = str(pos['start_date'])    
+        pos['start_date'] = str(pos['start_date'])
         pos['end_date'] = str(pos['end_date'])
-    return flask.render_template('campus_positions.html',
-        approved_group_ids=approved_group_ids, 
+    return flask.render_template(
+        'campus_positions.html',
+        approved_group_ids=approved_group_ids,
         approved_group_names=approved_group_names,
         all_positions=all_positions)
