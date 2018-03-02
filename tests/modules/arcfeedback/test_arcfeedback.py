2d93ca1a84d4a52499bd305240df164a87340cd2
davistardif <davoscavo@gmail.com>
2018-03-01 17:58:53 -0800

    Skele for tests

---
+++ tests/modules/arcfeedback/test_arcfeedback.py
@@ -0,0 +1,44 @@
+from donut.modules.arcfeedback import modules
+from donut.modules.arcfeedback import helpers
+from donut.testing.fixtures import client
+
+def test_get_link(client):
+    
+
+def test_get_id(client):
+
+
+def test_get_messages(client):
+    
+
+def test_get_summary(client):
+
+
+def test_get_course(client):
+
+
+def test_get_status(client):
+
+
+def test_get_emails(client):
+
+
+def test_get_all_fields(client):
+
+
+def get_new_posts(client):
+
+
+def test_register_complaint(client):
+
+
+def test_add_email(client):
+
+
+def test_add_message(client):
+    
+
+def test_api_add_message(client):
+
+
+def test_api_view_complaint(client):
