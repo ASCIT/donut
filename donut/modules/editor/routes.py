7b2b15893605fdca80c8184790b8595dd5e94bcb
Ziyan Mo <moziyan@yahoo.com>
2018-03-08 15:46:59 -0800

    Update Java script

--- donut/modules/editor/routes.py
+++ donut/modules/editor/routes.py
@@ -5,5 +5,5 @@ from donut.modules.committee_sites import blueprint, helpers
 
 
 @blueprint.route('/editor')
-def editor():
-    return flask.render_template('editor_page.html')
+def editor(input_text='Hello World!!!'):
+    return flask.render_template('editor_page.html', input_text=input_text)
--- donut/modules/editor/static/script.js
+++ donut/modules/editor/static/script.js
@@ -1,3 +1,4 @@
+
 function run() {
   var text = document.getElementById('source').value,
       target = document.getElementById('preview'),
@@ -7,10 +8,48 @@ function run() {
     target.innerHTML = html;
 }
 function save(){
-	
+
 }
 
 function change_title(){
  //var text = document.getElementById('title').value,
 
 }
+
+function insert_heading(size){
+  var string = "";
+  for(var i = 0; i< size; i++)
+  {
+    string += "#";
+  }
+  insert(string + "Heading_title" + string+ "\n");
+}
+
+function insert_italic(){
+  insert("*italicText*");
+}
+
+function insert_bold(){
+  insert("**boldText**");
+}
+
+function insert_strike_through(){
+  insert("~~strikeThroughText~~");
+}
+
+function insert_link(){
+  insert("[linkTitle](example.com)");
+}
+
+function insert_link(){
+  insert("[linkTitle](example.com)");
+}
+
+
+function insert(string){
+  var text = document.getElementById('source').value;
+  var txt2 = text.slice(0, document.getElementById('source').selectionStart)
+  + string + text.slice(document.getElementById('source').selectionStart);
+  document.getElementById('source').value = txt2;
+}
+
