5e9121a4feb462db5730ef75152e46a87ace9868
Ziyan Mo <moziyan@yahoo.com>
2018-03-08 16:02:17 -0800

    Make Lint

--- donut/modules/editor/__init__.py
+++ donut/modules/editor/__init__.py
@@ -1,4 +1,9 @@
 import flask
-blueprint = flask.Blueprint('editor', __name__, template_folder='templates', static_folder='static', static_url_path='/donut/modules/editor/static')
+blueprint = flask.Blueprint(
+    'editor',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/donut/modules/editor/static')
 
 import donut.modules.editor.routes
