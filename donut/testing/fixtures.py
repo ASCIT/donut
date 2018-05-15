69232352444fe0b601304d2ffd91c58a864131d8
Reng <googlypantsroberteng@gmail.com>
2018-05-14 18:01:26 -0700

    delete comments

--- donut/testing/fixtures.py
+++ donut/testing/fixtures.py
@@ -16,11 +16,6 @@ def client():
     # Establish an application context before running the tests.
     ctx = app.app_context()
     ctx.push()
-    #if 'DB_URI' in app.config:
-    #    engine = sqlalchemy.create_engine(
-    #        app.config['DB_URI'], convert_unicode=True)
-    #    flask.g.db = engine.connect()
-    #    flask.g.tx = flask.g.db.begin()
     if ('DB_NAME' in app.config and 'DB_USER' in app.config
             and 'DB_PASSWORD' in app.config):
         connection = pymysql.connect(
