21db14b7acb16d9714d195cd1352092606236d73
Thought-Weaver <loganapple540@gmail.com>
2018-10-23 16:49:52 -0700

    Added basic masquerading.

--- donut/modules/auth/routes.py
+++ donut/modules/auth/routes.py
@@ -12,14 +12,34 @@ def login():
 @blueprint.route('/login/submit', methods=['POST'])
 def login_submit():
     """Handle authentication."""
-    username = flask.request.form.get('username', None)
+    mask_and_user = flask.request.form.get('username', None)
     password = flask.request.form.get('password', None)
+
+    # Need to take care of the case when masquerading occurs.
+    username = None
+    mask = None
+
+    if mask_and_user is not None:
+        # Check for the character to split.
+        if ':' in mask_and_user:
+            split = mask_and_user.split(':')
+            username, mask = split[0], split[1]
+
+            if not helpers.masked_person_exists(mask):
+                raise Exception("No such person exists to mask.")
+        else:
+            username = mask_and_user
 
     if username is not None and password is not None:
         user_id = helpers.authenticate(username, password)
-        if user_id is not None:
-            permissions = auth_utils.get_permissions(username)
-            flask.session['username'] = username
+        if user_id is not None:            
+            if mask is not None and permissions['mask']:
+                flask.session['username'] = mask
+                permissions = auth_utils.get_permissions(mask)
+            else:
+                flask.session['username'] = username
+                permissions = auth_utils.get_permissions(username)
+            
             flask.session['permissions'] = permissions
             # True if there's any reason to show a link to the admin interface.
             flask.session[
