cc5c48c32dfe7c44734123de9863d1ac6df0ba0e
Caleb Sander <caleb.sander@gmail.com>
2018-10-23 18:06:15 -0700

    Add clientside planner table

--- donut/modules/courses/routes.py
+++ donut/modules/courses/routes.py
@@ -2,10 +2,13 @@ import flask
 
 from donut.modules.courses import blueprint, helpers
 
+YEARS = {1: 'Freshman', 2: 'Sophomore', 3: 'Junior', 4: 'Senior'}
+
 
 @blueprint.route('/planner')
 def planner():
-    return flask.render_template('planner.html')
+    return flask.render_template(
+        'planner.html', TERMS=helpers.TERM_NAMES, YEARS=YEARS)
 
 
 @blueprint.route('/scheduler')
