6625bdd6c839b83212701f07ad67180062d785a7
davistardif <davoscavo@gmail.com>
2018-10-23 18:11:45 -0700

    Add caleb and seans changes

--- tests/modules/groups/test_groups.py
+++ tests/modules/groups/test_groups.py
@@ -54,25 +54,31 @@ def test_get_group_positions_data(client):
 def test_get_position_holders(client):
     res = helpers.get_position_holders(5)
     assert len(res) == 2
-    res = [res[0]['first_name'], res[1]['first_name']]
-    assert 'Robert' in res and 'Sean' in res
+    assert set(row['first_name'] for row in res) == set(['Robert', 'Sean'])
+
     res = helpers.get_position_holders(1)
     assert len(res) == 2
-    res = [res[0]['first_name'], res[1]['first_name']]
-    assert 'David' in res and 'Robert' in res
+    assert set(row['first_name'] for row in res) == set(['Robert', 'David'])
+
     res = helpers.get_position_holders([1, 5])
     assert len(res) == 3
-    res = [res[0]['first_name'], res[1]['first_name'], res[2]['first_name']]
-    assert 'David' in res and 'Sean' in res and 'Robert' in res
+    assert set(row['first_name']
+               for row in res) == set(['Robert', 'Sean', 'David'])
 
 
 def test_get_positions_held(client):
     res = helpers.get_positions_held(4)
     assert len(res) == 2
     assert 4 in res and 5 in res
+
     res = helpers.get_positions_held(2)
     assert len(res) == 3
     assert 1 in res and 4 in res and 5 in res
+
+    res = helpers.get_positions_held(1)
+    assert len(res) == 1
+    assert 1 in res
+
     res = helpers.get_positions_held(-1)
     assert res == []
 
