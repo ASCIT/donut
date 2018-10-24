be7b9fb3ee19c1c1fd46c029486f50c26e3664fc
Thought-Weaver <loganapple540@gmail.com>
2018-10-23 17:50:46 -0700

    Removed arc feedback.

--- donut/modules/arcfeedback/__init__.py
+++
@@ -1,5 +0,0 @@
-import flask
-blueprint = flask.Blueprint(
-    'arcfeedback', __name__, template_folder='templates')
-
-import donut.modules.arcfeedback.routes
--- donut/modules/arcfeedback/email_templates.py
+++
@@ -1,7 +0,0 @@
-received_feedback = \
-"""
-We have received your feedback and the ARC will be notified.
-You can check the status of your feedback here: {0}\n
-Thanks!
-Donut Devteam
-"""
