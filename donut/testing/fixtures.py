178a319a0bcdd7b075dcc3775a4823b3402b41da
Caleb Sander <caleb.sander@gmail.com>
2019-06-05 16:26:27 -0700

    RIP SQLAlchemy

--- donut/testing/fixtures.py
+++ donut/testing/fixtures.py
@@ -1,7 +1,6 @@
 import pytest
 
 import flask
-import sqlalchemy
 import pymysql.cursors
 import donut
 from donut import app
@@ -17,14 +16,9 @@ def client():
     # Establish an application context before running the tests.
     ctx = app.app_context()
     ctx.push()
-    if 'DB_URI' in app.config:
-        engine = sqlalchemy.create_engine(
-            app.config['DB_URI'], convert_unicode=True)
-        flask.g.db = engine.connect()
-        flask.g.tx = flask.g.db.begin()
     if ('DB_NAME' in app.config and 'DB_USER' in app.config
             and 'DB_PASSWORD' in app.config):
-        connection = pymysql.connect(
+        flask.g.pymysql_db = pymysql.connect(
             host='localhost',
             database=app.config['DB_NAME'],
             user=app.config['DB_USER'],
@@ -33,10 +27,8 @@ def client():
             autocommit=True,
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor)
-        flask.g.pymysql_db = connection
 
     yield app.test_client()
+
     # Teardown logic (happens after each test function)
-    flask.g.tx.rollback()
-    flask.g.db.close()
     flask.g.pymysql_db.close()
