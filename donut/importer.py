d4940ef70d165100b02431bb88f75ad68047ef3f
Thought-Weaver <loganapple540@gmail.com>
2019-10-23 17:34:16 -0700

    Cleaned up importer.

--- donut/importer.py
+++ donut/importer.py
@@ -38,28 +38,20 @@ import sys
 
 from donut.pymysql_connection import make_db
 
+# -----------------------------------------
+# GLOBALS
+# -----------------------------------------
+
 # Create reference to DB
 db = make_db("dev")
 
-# Parse input arguments
-parser = argparse.ArgumentParser()
-parser.add_argument(
-        "file",
-        help="Path to registrar file, e.g. ascit_190927.txt")
-args = parser.parse_args()
-
-# Begin parsing TSV data
-data_list = list(csv.reader(open(args.file, "rb"), delimiter="\t"))
-
-if len(data_list) < 1:
-    # Raise some error. We at least need headers.
-    pass
-
-users = {}
-
-# We assume that the first row is the headers as specified above
-headers = data_list[0]
-
+# The following values are updated on duplicate keys:
+# msc, preferred_name
+# This were chosen because they're the only values that someone
+# might have update with or be updated by the Registrar that
+# warrant changing.
+# Note to self: See if you can specify a nickname on new Donut
+# and, if so, get rid of preferred_name from this update.
 query = """
     INSERT INTO members (
         uid,
@@ -80,8 +72,40 @@ query = """
         zip,
         country
     ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    ON DUPLICATE KEY uid = uid
+    ON DUPLICATE KEY UPDATE msc = msc, preferred_name = preferred_name
 """
+
+# -----------------------------------------
+# FUNCTIONS
+# -----------------------------------------
+
+def parse_data(file):
+    # Begin parsing TSV data
+    data_list = list(csv.reader(open(args.file, "rb"), delimiter="\t"))
+
+    if len(data_list) < 1:
+        raise Exception("There must at least exist headers in the file.")
+        return
+
+    users = {}
+    
+    # We assume that the first row is the headers as specified above
+    headers = data_list[0]
+
+    for i in range(1, len(data_list)):
+        row = dict(zip(headers, data_list[i]))
+        uid = row["UID"]
+        
+        if uid not in users:
+            try:
+                users[uid] = row
+            except e:
+                print("Error parsing student (ASCIT member)!", row, file=sys.stderr)
+                print(e, file=sys.stderr)
+
+    for user in users:
+        add_user_to_db(user)
+
 
 def add_user_to_db(user):
     with db.cursor() as cursor:
@@ -105,11 +129,14 @@ def add_user_to_db(user):
                 user["ZIP"],
                 user["COUNTRY"])
 
-for i in range(1, len(data_list)):
-    row = dict(zip(headers, data_list[i]))
-    try:
-        # Try to parse.
-        
-    except e:
-        print("Error parsing ASCIT member!", row, file=sys.stderr)
-        print(e, file=sys.stderr)
+
+if __name__ == "__main__":
+    # Parse input arguments
+    parser = argparse.ArgumentParser(description="Manages the importing of Registrar student data.")
+
+    parser.add_argument("file",
+                        help="Path to registrar file, e.g. ascit_190927.txt")
+    args = parser.parse_args()
+
+    parse_data(args.file)
+
