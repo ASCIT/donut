6d72cbadea21e28d2a7a698da4fc5d47ec34ee2e
Ziyan Mo <moziyan@yahoo.com>
2020-03-10 14:14:31 -0700

    Fix email x2

--- donut/email_utils.py
+++ donut/email_utils.py
@@ -10,7 +10,7 @@ def send_email(to, text, subject, use_prefix=True, group=None):
     is not none, the email is sent to a newsgroup and the to emails are hidden.
     """
 
-    msg = MIMEText(msg)
+    msg = MIMEText(text, "html")
     if use_prefix and '[ASCIT Donut]' not in subject:
         subject = '[ASCIT Donut] ' + subject
 
